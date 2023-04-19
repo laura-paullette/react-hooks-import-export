304d7e24499cda54cfc0134c098ee619829432ab
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-19 13:30:53 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,32 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife , elevation } from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  wildlife();
+  elevation();
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      {trees}
 
-  return <h1>Colorado State Parks!</h1>;
+    </div>
+
+
+  
+ 
+  )
+
+  
+  
 }
+
+  
+
+
+
+export default ColoradoStateParks
